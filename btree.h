161155e31ff4452f4e24ba349db197b066cd263e
Chris Brown <chris@thebrown.net>
2013-07-01 19:25:45 +0100

    First steps towards BTree

--- btree.h
+++ btree.h
@@ -1,4 +1,9 @@
 template<class K, class V>
 class BTree {
+	V value_;
 
+public:
+	V& operator[](const K& key) {
+		return value_;
+	}
 };
--- btree_tests.cc
+++ btree_tests.cc
@@ -1,6 +1,22 @@
 #include "btree.h"
 #include "gtest/gtest.h"
+#include <cstring>
 
-TEST(BTreeTest, CreateEmptyBTree) {
-	BTree<int,int> b;
+class Data {
+	char str_[100];
+public:
+	Data() {
+		str_[0] = 0;
+	}
+
+	Data(const char *str) {
+		strncpy(str_, str, 100);
+		str_[99] = 0;
+	}
+};
+
+TEST(BTreeTest, AssignKeyValuePair) {
+	BTree<int, Data> b;
+
+	b[4] = Data("test");
 }
