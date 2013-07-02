d43755942f652929e1fce5e1f01ca6b269505c0b
Chris Brown <chris@thebrown.net>
2013-07-02 19:25:27 +0100

    More BTree

--- btree.h
+++ btree.h
@@ -1,9 +1,41 @@
 template<class K, class V>
+struct Element {
+	Element* next;
+	K key;
+	V value;
+};
+
+template<class K, class V>
 class BTree {
-	V value_;
+	Element<K, V>* first_;
 
 public:
+	BTree() {
+		first_ = 0;
+	}
+
 	V& operator[](const K& key) {
-		return value_;
+		Element<K, V>* e = find(key);
+		if (e == 0) {
+			e = new Element<K, V>;
+			e->key = key;
+			insert(e);
+		}
+		return e->value;
+	}
+
+private:
+	Element<K, V>* find(const K& key) {
+		for (Element<K, V> *e = first_; e != 0; e = e->next) {
+			if (e->key == key) {
+				return e;
+			}
+		}
+		return 0;
+	}
+
+	void insert(Element<K, V> *e) {
+		e->next = first_;
+		first_ = e;
 	}
 };
--- btree_tests.cc
+++ btree_tests.cc
@@ -13,10 +13,24 @@ public:
 		strncpy(str_, str, 100);
 		str_[99] = 0;
 	}
+
+	const char* str() { return str_; }
 };
 
 TEST(BTreeTest, AssignKeyValuePair) {
 	BTree<int, Data> b;
 
 	b[4] = Data("test");
+
+	ASSERT_STREQ("test", b[4].str()) << "Expected 'test' to be in the BTree";
+}
+
+TEST(BTreeTest, AssignTwoKeyValuePairs) {
+	BTree<int, Data> b;
+
+	b[4] = Data("four");
+	b[9] = Data("nine");
+
+	ASSERT_STREQ("four", b[4].str()) << "Expected 4 to be mapped to 'four'";
+	ASSERT_STREQ("nine", b[9].str()) << "Expected 9 to be mapped to 'nine'";
 }
